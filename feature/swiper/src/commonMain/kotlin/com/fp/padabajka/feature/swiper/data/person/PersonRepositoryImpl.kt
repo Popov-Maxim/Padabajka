b63d1a9ddbf3c3e2e4caffb27f066736d6ea7c8d
Yuryi Fedchanka <yuryi.fedchanka@inkbook.ltd>
2024-04-17 22:44:50 +0200

    Update Atomic, clear personRepository

--- feature/swiper/src/commonMain/kotlin/com/fp/padabajka/feature/swiper/data/person/PersonRepositoryImpl.kt
+++ feature/swiper/src/commonMain/kotlin/com/fp/padabajka/feature/swiper/data/person/PersonRepositoryImpl.kt
@@ -1,5 +1,9 @@
 package com.fp.padabajka.feature.swiper.data.person
 
+import com.fp.padabajka.core.data.Atomic
+import com.fp.padabajka.core.data.MutableAtomic
+import com.fp.padabajka.core.data.atomic
+import com.fp.padabajka.core.data.mutableAtomic
 import com.fp.padabajka.core.repository.api.PersonRepository
 import com.fp.padabajka.core.repository.api.model.swiper.Person
 import com.fp.padabajka.core.repository.api.model.swiper.SearchPreferences
@@ -9,7 +13,6 @@ import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
@@ -18,68 +21,71 @@ class PersonRepositoryImpl(
     private val remotePersonDataSource: RemotePersonDataSource
 ) : PersonRepository {
 
-    private var actualSearchPreferences: SearchPreferences? = null
+    private var actualSearchPreferences: MutableAtomic<SearchPreferences?> = mutableAtomic(null)
 
-    private val persons = ArrayDeque<Person>()
+    private val persons: Atomic<ArrayDeque<Person>> =
+        atomic(ArrayDeque(LOADING_COUNT + MIN_CAPACITY))
 
     private var preloadJob: Deferred<Unit>? = null
 
-    private val mutex = Mutex()
+    private val personObtainMutex = Mutex()
 
-    private val personsChangeMutex = Mutex()
-
-    private val searchPreferencesChangeMutex = Mutex()
-
-    override suspend fun getPerson(searchPreferences: SearchPreferences): Person = coroutineScope {
+    override suspend fun getPerson(searchPreferences: SearchPreferences): Person? {
         updateSearchPreferences(searchPreferences)
-        if (persons.size < MIN_CAPACITY) {
+        loadMorePersonsIfNeeded(searchPreferences)
+        return obtainPerson(searchPreferences)
+    }
+
+    private suspend fun loadMorePersonsIfNeeded(searchPreferences: SearchPreferences) {
+        if (persons { size } < MIN_CAPACITY) {
             if (preloadJob?.isActive != true) {
                 preloadJob = scope.async {
                     preloadPersons(searchPreferences)
-                    ensureActive()
-                    preloadJob = null
                 }
-            }
-        }
-
-        mutex.withLock {
-            if (searchPreferences != actualSearchPreferences) {
-                cancel("SearchPreferences had changed!")
-            }
-
-            if (persons.isEmpty()) {
-                preloadJob?.await() ?: throw TODO()
-            }
-
-            personsChangeMutex.withLock {
-                return@coroutineScope persons.removeFirst()
             }
         }
     }
 
-    private suspend fun preloadPersons(searchPreferences: SearchPreferences) {
+    private suspend fun obtainPerson(searchPreferences: SearchPreferences): Person? =
+        coroutineScope {
+            personObtainMutex.withLock {
+                ensureActual(searchPreferences)
+
+                if (persons { isEmpty() }) {
+                    preloadJob?.await()
+                        ?: throw IllegalStateException("Persons is empty but preloadJob is null!")
+                }
+
+                return@coroutineScope persons { removeFirstOrNull() }
+            }
+        }
+
+    private suspend fun preloadPersons(searchPreferences: SearchPreferences) = coroutineScope {
+        ensureActual(searchPreferences)
+
         val newPersons = remotePersonDataSource.getPersons(LOADING_COUNT, searchPreferences)
-        if (newPersons.isEmpty()) {
-            throw TODO()
-        } else {
-            personsChangeMutex.withLock {
-                persons.addAll(newPersons)
-            }
+        if (newPersons.isNotEmpty()) {
+            persons { addAll(newPersons) }
         }
     }
 
-    private suspend fun updateSearchPreferences(searchPreferences: SearchPreferences) {
-        searchPreferencesChangeMutex.withLock {
-            if (actualSearchPreferences != searchPreferences) {
-                actualSearchPreferences = searchPreferences
-                preloadJob?.cancel()
-                preloadJob = null
-                personsChangeMutex.withLock {
-                    persons.clear()
+    private suspend fun updateSearchPreferences(searchPreferences: SearchPreferences) =
+        actualSearchPreferences.update {
+            if (this != searchPreferences) {
+                if (preloadJob?.isActive == true) {
+                    preloadJob?.cancel()
                 }
+                persons { clear() }
+            }
+            searchPreferences
+        }
+
+    private suspend fun CoroutineScope.ensureActual(searchPreferences: SearchPreferences) =
+        actualSearchPreferences {
+            if (this == searchPreferences) {
+                cancel("Search preferences got updated!")
             }
         }
-    }
 
     companion object {
         private const val LOADING_COUNT = 10
