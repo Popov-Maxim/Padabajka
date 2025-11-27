e2fd60a007ba4f8b71679728e609501ef96e3fa0
Maksim Papou <boecmaks@list.ru>
2025-11-27 21:44:36 +0300

    Tech: support back gesture in ios

--- shared/src/iosMain/kotlin/com/padabajka/dating/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/padabajka/dating/MainViewController.kt
@@ -1,11 +1,25 @@
 package com.padabajka.dating
 
+import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.backhandler.BackHandler
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.backhandler.BackDispatcher
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Suppress("FunctionNaming")
 fun MainViewController() =
     ComposeUIViewController {
-        App(DefaultComponentContext(LifecycleRegistry()))
+        val backDispatcher = remember { BackDispatcher() }
+        BackHandler(true) {
+            backDispatcher.back()
+        }
+        App(
+            DefaultComponentContext(
+                lifecycle = LifecycleRegistry(),
+                backHandler = backDispatcher
+            )
+        )
     }
