d011b563063cfd5809c3040a525516707bce1862
Maksim Papou <boecmaks@list.ru>
2026-06-22 22:37:47 +0300

    Subscription: trivial fix

--- feature/subscription/src/commonMain/kotlin/com/padabajka/dating/feature/subscription/data/billing/PurchaseResult.kt
+++ feature/subscription/src/commonMain/kotlin/com/padabajka/dating/feature/subscription/data/billing/PurchaseResult.kt
@@ -3,5 +3,4 @@ package com.padabajka.dating.feature.subscription.data.billing
 data class PurchaseResult(
     val productId: String,
     val purchaseToken: String? = null,
-    val transactionId: String? = null
 )
